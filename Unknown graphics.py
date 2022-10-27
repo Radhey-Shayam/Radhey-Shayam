74dcf739a48418a15a5e25a10c1287821bc90fd5
Radhey-Shayam <116826263+Radhey-Shayam@users.noreply.github.com>
2022-10-27 21:17:52 +0530

    Unknown graphics.py

---
+++ Unknown graphics.py
@@ -0,0 +1,14 @@
+import turtle
+sham=turtle.Turtle()
+sham.speed(78494994499464676454848848787878489494949448484884488484848484848484848488484848488484846161631342424534343434464664686867948480400448489464646646464646)
+sham.pensize(4)
+colors=["black","green","red","purple"]
+for i in range(5000000):
+	  sham.color(colors[i%4])
+	  sham.backward(90)
+	  sham.forward(i)
+	  sham.right(360.39)
+	  sham.left(90)
+	  sham.forward(87.94)
+	  sham.goto(0,10)
+	  sham.left(360)
